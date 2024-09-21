f908640f942aaa8e98dab00f43d8e5b53c9315a7
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-21 18:30:22 +0300

    Add expected types definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+type BlockType = 'image' | 'headline' | 'paragraph' | 'button';
+
+interface IConstructorBlock {
+  id: string;
+  type: BlockType;
+  title: string;
+  content: string;
+}
+
+type ReactChildrenType = React.ReactNode | React.ReactNode[];
